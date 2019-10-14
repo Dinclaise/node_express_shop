86527c7c0ed10a6f71d3619add3414715fa340c9
Anton <aanohin98@gmail.com>
2019-10-14 22:20:55 +0400

    0.5.0
    The 'data' folder has been deleted.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
+const User = require('./models/user');
 
 const app = express();
 
@@ -20,6 +21,18 @@ app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
+
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('5da49ce3ae0cb81a342d6dcb');
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log(e);
+  }
+  
+});
 
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
@@ -44,6 +57,15 @@ async function start() {
       useNewUrlParser: true,
       useFindAndModify: false
     });
+    const candidate = await User.findOne();
+    if (!candidate) {
+      const user = new User({
+        email: 'antoshka13@gmail.com',
+        name: 'Anton',
+        cart: {items: []}
+      });
+      await user.save();
+    }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
